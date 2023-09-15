1d64e778f6a6458550bc4bcf4af5310e71046106
Mark <mark.arias7@gmail.com>
2023-09-14 21:43:45 -0500

    Initial Commit

---
+++ DiceeInSwiftUI/DiceeInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeInSwiftUIApp.swift
+//  DiceeInSwiftUI
+//
+//  Created by Mark Arias on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceeInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
